4d735986f0ff6db2a8707bb01820add917232ee9
Emilien <emiliengir@gmail.com>
2024-01-30 16:32:33 +0100

    retour Karl de l'enfer

--- routes/trips.js
+++ routes/trips.js
@@ -6,9 +6,25 @@ require('../models/connection');
 const Trip = require('../models/trips');
 
 router.get('/:departure/:arrival/:date', (req, res) => {
+
+    let dateFmt = new Date(req.params.date);
+
+    console.log(dateFmt)
     Trip
-    .find({departure: req.params.departure.toLowerCase(), arrival: req.params.arrival.toLowerCase(), date: new Date(req.params.date)})
-    .then((data) => res.json({ result: true, trip: data }))
+        .find({
+            departure: req.params.departure,
+            arrival: req.params.arrival,
+            date: { $gte: dateFmt }
+        })
+        .then((trips) => {
+            console.log(trips)
+            if (trips.length !== 0) {
+                res.json({ result: true, trips })
+            } else {
+                res.json({ result: false })
+            }
+        })
+        .catch((error) => console.error(error))
 })
 
 module.exports = router;
